7bee7ef8382f38db7df5e69b3bec7037866e14ab
multimikan <koki20041008@gmail.com>
2025-12-01 21:35:54 +0900

    s

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from start import view as s
 import component as c
 import app.main_controller as controller
+import os
 
 class __main__():
     def __init__(self):
         self.controller = controller.Controller()
+        self.start_view = s.View(self.controller)
     def app(self):
        while self.controller.getIsActive():
-            c.print_view(s.View(self.controller).view(),footer="操作方法：wasd, z:enter, x:back")
+            c.print_view(self.start_view.view(),footer="操作方法：wasd, z:enter, x:back")
             self.controller.input_controller(input("入力:"))
-            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n")   
+            os.system('cls' if os.name == 'nt' else 'clear')
 
 
 __main__().app()
